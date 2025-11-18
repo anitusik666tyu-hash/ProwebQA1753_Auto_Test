2b91a7cdd4d0bfe868c771893c760d08f46906e9
anitusik666tyu-hash <anitusik666tyu@gmail.com>
2025-11-18 18:20:50 +0500

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,19 +3,7 @@ from selenium import webdriver
 
 @pytest.fixture
 def driver_chrome():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from selenium.webdriver.chrome.options import Options
-
-    options = Options()
-    options.add_argument("==no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-
-    driver = webdriver.Chrome(
-        service = Service(ChromeDriverManager().install()),
-        options = options
-    )
+    driver = webdriver.Chrome()
     driver.maximize_window()
     driver.implicitly_wait(10)
     yield driver
