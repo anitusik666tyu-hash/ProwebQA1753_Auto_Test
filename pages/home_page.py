4dbc20191a122cf97ad4f2eae08f35ad2f06b640
anitusik666tyu-hash <anitusik666tyu@gmail.com>
2025-11-18 17:47:23 +0500

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -3,6 +3,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
+options = Options()
+options.add_argument("==no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+driver = webdriver.Chrome(
+    service = Service(ChromeDriverManager().install()),
+    options = options
+)
+
 class HomePage:
     def __init__(self, driver):
         self.driver = driver
